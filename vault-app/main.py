1058bd92cd61d05e3d401031c55d7fb4a487a99e
klidnenabrezi <158766194+klidnenabrezi@users.noreply.github.com>
2026-04-30 00:06:48 +0700

    nyoba nyoba aja sih

---
+++ vault-app/main.py
@@ -0,0 +1,10 @@
+print("Welcome to BrezVault")
+print("Please choose the action you want to perform: ")
+print("1. Insert a new account")
+
+#Option 1
+print("Provide a new account name: ")
+print("Provide a new account password: ")
+print("Provide the master key: ")
+print("Encrypting...")
+print("Encryption complete")
